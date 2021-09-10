b84bd15fb1ade5b32bf0e7d6826f73977d5744ab
Chin Yu <chin.yuchiuj@gmail.com>
2021-09-10 20:53:19 +0800

    refactor

--- products.py
+++ products.py
@@ -1,36 +1,52 @@
 import os # operating sys
 
-
-#　read the file
-products = []
-if os.path.isfile('products.csv'): # ensure the existence of the file
-	print('yeahh! the file is found')
-	with open('products.csv', 'r', encoding = 'utf-8') as f:
-		for line in f:
-			if 'Product, Price' in line:
-				continue
-			name, price = line.strip(). split(',')
-			products.append([name, price])
-	print(products)
-else:
-	print('the file can not be found')
-
+def read_file(filename):
+    products = []
+    with open(filename, 'r', encoding = 'utf-8') as f:
+        for line in f:
+            if 'Product, Price' in line:
+                continue  # next round
+            name, price = line.strip(). split(',')
+            products.append([name, price])
+    return products
+          
 # let the user to enter
-while True:
-	name = input('enter the name of product: ') 
-	if name == 'q':
-		break
-	price = input('enter the price of product: ')
-	price = int(price)
-	products.append([name, price]) # p = [name, price], # products.append(p)
-print(products)
+def user_input(products):
+    while True:
+        name = input('enter the name of product: ')
+        if name == "q":
+            break
+        price = input('enter the price of product: ')
+        price = int(price)
+        products.append([name, price]) # p = [name, price], # products.append(p)
+    print(products)
+    return products
 
 # print the entire purchase record
-for p in products:
-	print(p[0], 'cost', p[1]) 
+def print_products(products):
+    for p in products:
+        print(p[0], 'cost', p[1]) 
 
 # write the file
-with open('products.csv', 'w', encoding = 'utf-8') as f:
-	f.write('Product, Price\n')
-	for p in products:
-		f.write(p[0] + ',' + str(p[1]) + '\n') #string
+def write_file(filename, products):
+    with open(filename, 'w', encoding = 'utf-8') as f:
+        f.write('Product, Price\n')
+        for p in products:
+            f.write(p[0] + ',' + str(p[1]) + '\n') #string
+
+
+def main():
+    filename = 'products.csv'
+    if os.path.isfile(filename): # ensure the existence of the file
+        print('yeahh! the file is found')
+        products = read_file(filename)
+    else:
+        print('the file can not be found')
+
+    products = user_input(products)
+    print_products(products)
+    write_file('products.csv', products)
+
+main()
+
+#refactor reestructurar
